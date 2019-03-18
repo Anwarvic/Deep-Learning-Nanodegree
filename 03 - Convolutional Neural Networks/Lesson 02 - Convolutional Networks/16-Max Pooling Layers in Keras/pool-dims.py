01d7dc15bd93df10bc5b22cc501ca11033e7b9ea
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-03-18 13:35:05 +0200

    Third module, second lesson, third quiz: adding some code

---
+++ 03 - Convolutional Neural Networks/Lesson 02 - Convolutional Networks/16-Max Pooling Layers in Keras/pool-dims.py
@@ -0,0 +1,6 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import MaxPooling2D
+
+model = Sequential()
+model.add(MaxPooling2D(pool_size=2, strides=2, input_shape=(100, 100, 15)))
+model.summary()
