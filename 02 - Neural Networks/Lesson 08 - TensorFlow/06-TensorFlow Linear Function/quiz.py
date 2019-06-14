6ed4397d1579981170e0865e425b52dc7c452538
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-14 02:05:45 +0200

    Second Lesson, Last lesson: adding third quiz

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/06-TensorFlow Linear Function/quiz.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+def get_weights(n_features, n_labels):
+    """
+    Return TensorFlow weights
+    :param n_features: Number of features
+    :param n_labels: Number of labels
+    :return: TensorFlow weights
+    """
+    # TODO: Return weights
+    pass
+
+
+def get_biases(n_labels):
+    """
+    Return TensorFlow bias
+    :param n_labels: Number of labels
+    :return: TensorFlow bias
+    """
+    # TODO: Return biases
+    pass
+
+
+def linear(input, w, b):
+    """
+    Return linear function in TensorFlow
+    :param input: TensorFlow input
+    :param w: TensorFlow weights
+    :param b: TensorFlow biases
+    :return: TensorFlow linear function
+    """
+    # TODO: Linear Function (xW + b)
+    pass
