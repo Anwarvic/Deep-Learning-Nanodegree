2dd2577221348faffcc7e4db0b967df474642872
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-15 02:08:03 +0200

    Second Lesson, Last lesson: adding quiz of seventh part

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/07-TensorFlow Softmax/quiz.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+def run():
+    output = None
+    logit_data = [2.0, 1.0, 0.1]
+    logits = tf.placeholder(tf.float32)
+    
+    # TODO: Calculate the softmax of the logits
+    # softmax =     
+    
+    with tf.Session() as sess:
+        # TODO: Feed in the logit data
+        # output = sess.run(softmax,    )
+
+    return output
