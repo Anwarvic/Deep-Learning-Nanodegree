61872b4991cb4f177bf5b3d9a1bdd47c3d583dd6
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-16 02:10:04 +0200

    Second Lesson, Last lesson: adding quiz (8)

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/08-TensorFlow Cross Entropy/quiz.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+softmax_data = [0.7, 0.2, 0.1]
+one_hot_data = [1.0, 0.0, 0.0]
+
+softmax = tf.placeholder(tf.float32)
+one_hot = tf.placeholder(tf.float32)
+
+# TODO: Print cross entropy from session
