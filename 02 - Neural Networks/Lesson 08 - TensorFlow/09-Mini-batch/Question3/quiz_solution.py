5929f84b7bc7a3400fe4fce32ad33ac9ec78b095
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-18 02:27:09 +0200

    Second Lesson, Last lesson: adding third question (9) answer

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/09-Mini-batch/Question3/quiz_solution.py
@@ -0,0 +1,36 @@
+import math
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    # first for features
+    output = []
+    for idx in range(0, len(features), batch_size):
+        tmp = [features[idx:idx+batch_size], labels[idx:idx+batch_size]]
+        output.append(tmp)
+    return output
+        
+
+from pprint import pprint
+
+# 4 Samples of features
+example_features = [
+    ['F11','F12','F13','F14'],
+    ['F21','F22','F23','F24'],
+    ['F31','F32','F33','F34'],
+    ['F41','F42','F43','F44']]
+# 4 Samples of labels
+example_labels = [
+    ['L11','L12'],
+    ['L21','L22'],
+    ['L31','L32'],
+    ['L41','L42']]
+
+# PPrint prints data structures like 2d arrays, so they are easier to read
+pprint(batches(3, example_features, example_labels))
