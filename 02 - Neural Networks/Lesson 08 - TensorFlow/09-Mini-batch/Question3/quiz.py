f7eb9ac79b8eaf6c50104cfe9dee57545bcf0e91
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-18 02:26:28 +0200

    Second Lesson, Last lesson: adding third question (9)

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/09-Mini-batch/Question3/quiz.py
@@ -0,0 +1,12 @@
+import math
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    pass
