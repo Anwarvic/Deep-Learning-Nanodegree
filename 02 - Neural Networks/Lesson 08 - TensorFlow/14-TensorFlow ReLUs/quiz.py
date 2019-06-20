14b33086e6b165435dcf8039896870c062395813
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-20 02:31:23 +0200

    Second Lesson, Last lesson: (14) adding quiz

---
+++ 02 - Neural Networks/Lesson 08 - TensorFlow/14-TensorFlow ReLUs/quiz.py
@@ -0,0 +1,28 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+output = None
+hidden_layer_weights = [
+    [0.1, 0.2, 0.4],
+    [0.4, 0.6, 0.6],
+    [0.5, 0.9, 0.1],
+    [0.8, 0.2, 0.8]]
+out_weights = [
+    [0.1, 0.6],
+    [0.2, 0.1],
+    [0.7, 0.9]]
+
+# Weights and biases
+weights = [
+    tf.Variable(hidden_layer_weights),
+    tf.Variable(out_weights)]
+biases = [
+    tf.Variable(tf.zeros(3)),
+    tf.Variable(tf.zeros(2))]
+
+# Input
+features = tf.Variable([[1.0, 2.0, 3.0, 4.0], [-1.0, -2.0, -3.0, -4.0], [11.0, 12.0, 13.0, 14.0]])
+
+# TODO: Create Model
+
+# TODO: Print session results
